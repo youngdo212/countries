2f0e7a4fb648511d84f8d9d45a4273061d3ea6c8
mando <nsa06031@gmail.com>
2021-02-06 18:12:10 +0900

    feat: Table 컴포넌트 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,49 @@
-import * as React from "react";
-import styled from 'styled-components';
+import * as React from 'react';
+import Table from './components/Table';
 
-const Container = styled.h1`
-  color: #99f;
-`;
-
-function App() {
-  return <Container>Hello, Typescript!</Container>;
-}
+const App = (): JSX.Element => {
+  const columms = [
+    {
+      title: 'name',
+      dataIndex: 'name',
+      onClick: () => {
+        alert('name is clicked!');
+      },
+    },
+    {
+      title: 'age',
+      dataIndex: 'age',
+    },
+    {
+      title: 'live',
+      dataIndex: 'live',
+    },
+  ];
+  const data = [
+    {
+      key: 1,
+      name: 'mando',
+      age: 29,
+      live: 'seoul',
+    },
+    {
+      key: 2,
+      name: 'crong',
+      age: 40,
+      live: 'korea',
+    },
+    {
+      key: 3,
+      name: 'shlee',
+      age: 30,
+      live: 'mapo',
+    },
+  ];
+  return (
+    <div>
+      <Table columns={columms} dataSource={data} />
+    </div>
+  );
+};
 
 export default App;
