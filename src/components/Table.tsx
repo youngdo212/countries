6aa2660101caa6074aea28d4bdc26265731af4b2
mando <nsa06031@gmail.com>
2021-02-06 18:45:31 +0900

    feat: Table 컴포넌트의 onClick prop이 colume을 인자로 받도록 변경

--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -1,26 +1,30 @@
 import * as React from 'react';
 import styled from 'styled-components';
 
-export interface TableProps {
+interface TableProps {
   /** 테이블의 컬럼 값 */
-  columns: {
-    /** 컬럼의 이름 */
-    title: string;
-    /** 컬럼에 해당되는 data의 속성 */
-    dataIndex: string;
-    /** 컬럼을 고유하게 식별하는 값. key가 없을 경우 dataIndex를 사용 */
-    key?: string | number;
-    /** data[dataIndex]를 값으로 받아 렌더링을 수행하는 함수 */
-    render?: (dataValue: unknown) => React.ReactNode;
-    /** 컬럼이 클릭되었을 때 실행할 콜백 함수 */
-    onClick?: React.MouseEventHandler;
-  }[];
+  columns: TableColumn[];
   /** 테이블에 표시할 row의 집합 */
-  dataSource: {
-    /** data를 고유하게 식별하는 값 */
-    key: string | number;
-    [property: string]: React.ReactNode;
-  }[];
+  dataSource: TableData[];
+}
+
+export interface TableColumn {
+  /** 컬럼의 이름 */
+  title: string;
+  /** 컬럼에 해당되는 data의 속성 */
+  dataIndex: string;
+  /** 컬럼을 고유하게 식별하는 값. key가 없을 경우 dataIndex를 사용 */
+  key?: string | number;
+  /** data[dataIndex]를 값으로 받아 렌더링을 수행하는 함수 */
+  render?: (dataValue: unknown) => React.ReactNode;
+  /** 컬럼이 클릭되었을 때 실행할 콜백 함수 */
+  onClick?: (column: TableColumn) => void;
+}
+
+interface TableData {
+  /** data를 고유하게 식별하는 값 */
+  key: string | number;
+  [property: string]: React.ReactNode;
 }
 
 /** 테이블 컴포넌트 */
@@ -29,15 +33,18 @@ const Table = ({ columns, dataSource }: TableProps): JSX.Element => {
     <TableWrapper>
       <TableHeader>
         <TabularRow>
-          {columns.map(({ title, dataIndex, key, onClick }) => (
-            <TabularHeader
-              key={key || dataIndex}
-              clickable={!!onClick}
-              onClick={onClick}
-            >
-              {title}
-            </TabularHeader>
-          ))}
+          {columns.map((column) => {
+            const { title, key, dataIndex, onClick } = column;
+            return (
+              <TabularHeader
+                key={key || dataIndex}
+                clickable={!!onClick}
+                onClick={() => onClick && onClick(column)}
+              >
+                {title}
+              </TabularHeader>
+            );
+          })}
         </TabularRow>
       </TableHeader>
       <TableBody>
